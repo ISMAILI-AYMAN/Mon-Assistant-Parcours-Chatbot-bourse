74626e6b55716646153f3d1cafbf004f05e568e6
ISMAILI-AYMAN <ismailiayman1@gmail.com>
2024-12-24 11:49:25 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,6 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import sphinx_rtd_theme
 project = 'MON ASSISTANT PARCOURS CHATBOT BOURSE'
 copyright = '2024, ISMAILI AYMAN AND AKAKPO Koffi Moïse'
 author = 'ISMAILI AYMAN AND AKAKPO Koffi Moïse'
@@ -14,7 +13,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +23,5 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
